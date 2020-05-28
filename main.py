6d92eaf23d55c45aee4297e3f256ae95b00d1549
troshha <troshha@gmail.com>
2020-05-28 22:09:08 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,96 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty
+import csv
+
+
+api_id = 1038476
+api_hash = 'c8e3a76798991d9efa731038409ebc55'
+phone = str(input('Enter the phone number: '))
+client = TelegramClient(phone, api_id, api_hash)
+
+
+def connectClient():
+    client.connect()
+    if not client.is_user_authorized():
+        client.send_code_request(phone)
+        client.sign_in(phone, input('Enter the code: '))
+
+
+def scrapeGroups():
+    chats = []
+    last_date = None
+    chunk_size = 200
+    groups = []
+
+    result = client(GetDialogsRequest(
+        offset_date=last_date,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=chunk_size,
+        hash=0
+    ))
+    chats.extend(result.chats)
+
+    for chat in chats:
+        try:
+            if chat.megagroup == True:
+                groups.append(chat)
+        except:
+            continue
+
+    print('Choose a group to scrape members from:')
+    i = 0
+    for g in groups:
+        print(str(i) + '- ' + g.title)
+        i += 1
+
+    g_index = input("Enter a Number: ")
+    target_group = groups[int(g_index)]
+
+    print('Fetching Members...')
+    all_participants = []
+    all_participants = client.get_participants(target_group, aggressive=True)
+    usernames = []
+    for participant in all_participants:
+        if participant.username is not None:
+            usernames.append(participant.username)
+
+        else:
+            usernames.append('User type is none')
+    client.send_message(
+        'me', f"List of all users From {target_group.title}.\n Total user count {len(usernames)}")
+    client.send_message('me', "\n".join(usernames))
+
+
+def main():
+    connectClient()
+    scrapeGroups()
+    # print('Saving In file...')
+    # with open("members.csv", "w", encoding='UTF-8') as f:
+    #     writer = csv.writer(f, delimiter=",", lineterminator="\n")
+    #     writer.writerow(['username', 'user id', 'access hash',
+    #                      'name', 'group', 'group id'])
+    #     for user in all_participants:
+    #         if user.username:
+    #             username = user.username
+    #         else:
+    #             username = ""
+    #         if user.first_name:
+    #             first_name = user.first_name
+    #         else:
+    #             first_name = ""
+    #         if user.last_name:
+    #             last_name = user.last_name
+    #         else:
+    #             last_name = ""
+    #         name = (first_name + ' ' + last_name).strip()
+    #         writer.writerow([username, user.id, user.access_hash,
+    #                          name, target_group.title, target_group.id])
+    # # client.send_file('me', './members.csv',
+    # #                  caption='Members scraped successfully.')
+    print('Members scraped successfully.')
+
+
+if __name__ == '__main__':
+    main()
